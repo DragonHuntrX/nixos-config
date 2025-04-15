b1e4f16f08c7c62ebdf270dc566057ab9bfa8d17
DragonHuntrX <John@Devost.net>
2025-04-15 10:25:57 -0400

    Added nvidia prime

--- gpusetup.nix
+++ gpusetup.nix
@@ -44,4 +44,16 @@
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 
+  hardware.nvidia.prime = {
+    offload = {
+      enable = true;
+      enableOffloadCmd = true;
+    };
+
+    # Make sure to use the correct Bus ID values for your system!
+    intelBusId = "PCI:0:2:0";
+    nvidiaBusId = "PCI:1:0:0";
+    # amdgpuBusId = "PCI:54:0:0"; For AMD GPU
+  };
+
 }
